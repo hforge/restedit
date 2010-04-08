8a2c8a82a15d7df63543acd3b9fd89437402881e
David Versmisse <david.versmisse@itaapy.com>
2010-04-08 09:12:04 +0200

    Remove the version of Python

--- zopeedit.py
+++ zopeedit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.5
+#!/usr/bin/env python
 ##############################################################################
 #
 # Copyright (c) 2001, 2002 Zope Corporation and Contributors.
