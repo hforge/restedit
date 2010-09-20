10a4ccb3e388feec72de072ee7ed1b472b46ed54
David Versmisse <david.versmisse@itaapy.com>
2010-09-20 12:25:13 +0200

    Now we can make the setup.exe file for 32bits or 64bits

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from distutils.core import setup
+from platform import architecture
+from shutil import copyfile
 import py2exe
 
-setup(data_files=["msvcr90.dll"],
-      windows=['restedit.py'])
+# Make the dist directory 
+setup(windows=['restedit.py'])
+
+# Copy the good vcredist file
+if architecture()[0].startswith('32'):
+    copyfile('vcredist_x86.exe', 'dist/vcredist_x86.exe')
+else:
+    copyfile('vcredist_x64.exe', 'dist/vcredist_x64.exe')
+
 
